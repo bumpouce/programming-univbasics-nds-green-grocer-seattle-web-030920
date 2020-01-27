37696bbe653f6afa0652883659f5704e90a13d31
Christine Bumpous <bumpouce@gmail.com>
2020-01-27 18:16:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,7 @@ def apply_coupons(cart, coupons)
     if search_item[:count] < coupons[i][:num]
         pp "Not enough #{coupons[i][:item]}s to apply coupon."
         pp "Initial coupon: #{coupons[i]}"
-        coupons[i][:item] = "#{coupons[i][:item]} W/ COUPON"
+        coupons[i][:item] = coupons[i][:item] && " W/ COUPON"
         coupons[i][:price] = coupons[i][:price] / coupons[i][:num]
         coupons[i][:clearance] = true 
         pp "Transformed coupon: #{coupons[i]}"
