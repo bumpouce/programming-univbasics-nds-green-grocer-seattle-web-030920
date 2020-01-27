2b15d5de89007193bc7889d4dccc3dc064b98b2d
Christine Bumpous <bumpouce@gmail.com>
2020-01-27 18:25:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -61,7 +61,7 @@ def apply_coupons(cart, coupons)
         pp "Not enough #{coupons[i][:item]}s to apply coupon."
         pp "Initial coupon: #{coupons[i]}"
         
-        coupon_item = coupons[i][:item] && " W/ COUPON"
+        cart << :item => coupons[i][:item] && " W/ COUPON"
 #        individual_price = coupons[i][:price] / coupons[i][:num]
 #        clearance_flag = true
         
